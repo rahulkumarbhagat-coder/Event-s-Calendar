1b4b922913f10561c1ae18233624c11c72ce8482
rahulkumarbhagat-coder <bhagatrahul847@gmail.com>
2024-12-13 00:03:34 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcssAnimate from "tailwindcss-animate";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcssAnimate],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
