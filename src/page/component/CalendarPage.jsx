24ec0168696c5c3049066aba9d83199e0242994e
rahulkumarbhagat-coder <bhagatrahul847@gmail.com>
2024-12-14 18:04:40 +0530

    added localstorage

--- src/page/component/CalendarPage.jsx
+++ src/page/component/CalendarPage.jsx
@@ -1,11 +1,13 @@
 import { Calendar } from "../../components/ui/calendar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddEvent from "./AddEvent";
 
 const CalendarPage = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [events, setEvents] = useState({});
+  const [isfetched, setIsFetched] = useState(false);
 
+  //To add an event
   const handleAddEvent = (newEvent) => {
     if (!selectedDate) {
       alert("Please select a date before adding an event.");
@@ -17,6 +19,33 @@ const CalendarPage = () => {
       [dateKey]: [...(prevEvents[dateKey] || []), newEvent],
     }));
   };
+
+  //To delete an event
+  const handleDelete = (dateKey, eventIndex) => {
+    setEvents((prevEvents) => {
+      const updatedEvents = { ...prevEvents };
+      updatedEvents[dateKey] = updatedEvents[dateKey].filter((_, index) => {
+        index !== eventIndex;
+      });
+      if (updatedEvents[dateKey].length == 0) {
+        delete updatedEvents[dateKey];
+      }
+      return updatedEvents;
+    });
+  };
+
+  //To save an event
+  useEffect(() => {
+    const storeEvents = localStorage.getItem("events");
+    if (storeEvents) {
+      setEvents(JSON.parse(storeEvents));
+      setIsFetched(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("events", JSON.stringify(events));
+  }, [events,isfetched]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-blue-400">
@@ -32,21 +61,33 @@ const CalendarPage = () => {
           <p>
             Selected Date: <strong>{selectedDate.toLocaleDateString()}</strong>
           </p>
-          <div className="mt-2">
-            {events[selectedDate.toDateString()] ? (
-              events[selectedDate.toDateString()].map((events, index) => (
-                <div key={index} className="p-2 border rounded mb-2 bg-white">
-                  <h3 className="font-bold">{events.eventName}</h3>
-                  <p>
-                    {events.startTime} - {events.endTime}
-                  </p>
-                  <p>{events.description}</p>
-                </div>
-              ))
-            ) : (
-              <p>No events for this date.</p>
-            )}
-          </div>
+          {isfetched ? (
+            <div className="mt-2">
+              {events[selectedDate.toDateString()] ? (
+                events[selectedDate.toDateString()].map((events, index) => (
+                  <div key={index} className="p-2 border rounded mb-2 bg-white">
+                    <h3 className="font-bold">{events.eventName}</h3>
+                    <p>
+                      {events.startTime} - {events.endTime}
+                    </p>
+                    <p>{events.description}</p>
+                    <button
+                      onClick={() =>
+                        handleDelete(selectedDate.toDateString(), index)
+                      }
+                      className="text-red-500 hover:underline"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <p>No events for this date.</p>
+              )}
+            </div>
+          ) : (
+            "...Loading"
+          )}
         </div>
       )}
       <AddEvent onSave={handleAddEvent} />
